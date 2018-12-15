5d9747adb69759438f2a1add3f6ecc55a0af0c78
sunghyunpark <sunghyun1038@naver.com>
2018-12-15 19:47:45 +0900

    add app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+  extended:false
+}));
+app.use(bodyParser.json());
+
+app.use(function(erq, res, next){
+  res.header('Access-Control-Allow_Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'content-type, x-access-token');
+  next();
+});
+
+app.use('/api/stockSaying', require('./routes/stockSaying'));
+
+
+app.use(function(req, res, next){
+  res.status(404);
+  res.json({
+    error : 'Not Found'
+  });
+  return;
+});
+
+app.listen(1038, function(){
+  console.log('Connected, StockSaying port!');
+})
