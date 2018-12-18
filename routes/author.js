b3a5373899dbfd6338d161bb230ec429ede3bee7
sunghyunpark <sunghyun1038@naver.com>
2018-12-18 16:23:43 +0900

    작가 등록 시 중복 체크하여 등록되게 수정

--- routes/author.js
+++ routes/author.js
@@ -22,22 +22,20 @@ router.post('/upload/author', function(req, res){
   var authorName = req.body.authorName;
   var currentTime = new Date().toFormat('YYYY-MM-DD HH24:MI:SS');
 
-  console.log('author called');
-
   var sql = 'SELECT author_name FROM author WHERE author_name = ?';
   conn.query(sql, [authorName], function(err, result, fields){
     if(err){
-      console.log('select err');
+      console.log(err);
     }else{
-      console.log(result.length+'개');
       if(result.length > 0){
+        // 동일한 작가가 있는 경우
         res.json(responseUtil.successFalse(500, '이미 등록한 작가가 존재합니다.'));
       }else{
+        // 동일한 작가가 없는 경우
         var sql = 'INSERT INTO author (author_name, created_at) VALUES(?, ?)';
         conn.query(sql, [authorName, currentTime], function(err, result, fields){
           if(err){
             console.log(err);
-            res.json(responseUtil.successFalse(500, '이미 등록한 작가가 존재합니다.'));
           }else{
             res.json(responseUtil.successTrue());
             console.log('Success to register author');
