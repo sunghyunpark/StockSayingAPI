e54884e3af52b156c1102b4636634c4a2649dce8
sunghyunpark <sunghyun1038@naver.com>
2018-12-19 15:41:45 +0900

    api test

--- routes/saying.js
+++ routes/saying.js
@@ -90,7 +90,7 @@ router.get('/list/saying/:no/:sort', function(req, res){
   'gravity_horizontal AS gravityHorizontal, '+
   'gravity_vertical AS gravityVertical, '+
   'created_at AS createdAt '+
-  'FROM article '+ offset + ' ORDER BY created_at DESC LIMIT 10';
+  'FROM article '+ offset + ' ORDER BY created_at DESC LIMIT 30';
 
   conn.query(sql, [sort], function(err, result, fields){
     if(err){
