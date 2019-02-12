dd88c1f83a7f985c146c2b213a96c736ee02b1f5
sunghyunpark <sunghyun1038@naver.com>
2019-02-12 16:52:46 +0900

    저자 리스트 불러오기 롤백

--- routes/author.js
+++ routes/author.js
@@ -107,12 +107,12 @@ router.put('/swap/author', function(req, res){
 */
 router.get('/authorList/:no', function(req, res){
   var no = req.params.no;
-  var offsetSql = (no == 0) ? '' : ' WHERE author.no >' + no;
+  var offsetSql = (no == 0) ? '' : ' WHERE author.created_at < (SELECT created_at FROM author WHERE no='+no+')';
 
   var sql = 'SELECT author_name AS authorName, '+
   'no, '+
   'created_at AS createdAt'+
-  ' FROM author' + offsetSql + ' ORDER BY no ASC LIMIT 30';
+  ' FROM author' + offsetSql + ' ORDER BY created_at DESC LIMIT 30';
 
   conn.query(sql, [], function(err, result, fields){
     if(err){
